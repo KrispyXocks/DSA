5a350de1222673337a3e6ac115bb2878263c065b
VNeseTalkingBush <95459496+KrispyXocks@users.noreply.github.com>
2023-05-25 06:57:10 +0700

    update

--- DSA/src/Binary_method/BinaryTree.java
+++ DSA/src/Binary_method/BinaryTree.java
@@ -1,36 +1,109 @@
 package Binary_method;
+
 public class BinaryTree {
-    public TreeNode arrayToBinaryTree(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return null;
+    public static void main(String[] args) {
+        // 4, 2, 8, 3, 1, 7, 9, 6, 5
+        int[]   arr = { 4, 2, 8, 3, 1, 7, 9, 6, 5 };
+
+        // tree insert
+        BST tree = new BST(arr[0]);
+        for(int i = 1; i < arr.length; i++) {
+            tree.insert(new BSTNode(arr[i]));
         }
 
-        return constructTree(nums, 0, nums.length - 1);
-    }
-
-    private TreeNode constructTree(int[] nums, int start, int end) {
-        if (start > end) {
-            return null;
-        }
-
-        int mid = (start + end) / 2;
-        TreeNode node = new TreeNode(nums[mid]);
-
-        node.left = constructTree(nums, start, mid - 1);
-        node.right = constructTree(nums, mid + 1, end);
-
-        return node;
+        tree.inOrder();
+        // Let's do some search
+        tree.search(4);
+        tree.search(3);
+        tree.search(6);
+        tree.search(10);
     }
 }
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+class BSTNode {
+    int data; // data can be anything as long as you can compare two data
+    BSTNode left;
+    BSTNode right;
 
-    TreeNode(int val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
+    public BSTNode(int data) {
+        this.data = data;
+        left = right = null;
+    }
+
+    public void addChild(BSTNode newNode) {
+        // add a new node as a new child of the current node
+        // but if the current node has a child at the same location already
+        // call this method recursively to the child
+        if (newNode.data == data) {
+            // duplicated data => do nothing
+            return;
+        }
+        if (newNode.data < data) {
+            if (left == null) {
+                left = newNode;
+            } else {
+                left.addChild(newNode);
+            }
+        } else {
+            if (right == null) {
+                right = newNode;
+            } else {
+                right.addChild(newNode);
+            }
+        }
+    }
+}
+
+class BST {
+    BSTNode root;
+
+    public BST(int data) {
+        root = new BSTNode(data);
+    }
+
+    public void insert(BSTNode newNode) {
+        root.addChild(newNode);
+    }
+
+    public void inOrder() {
+        // start the inOrder traversal from root
+        inOrderRecursive(root);
+    }
+
+    public void visit(BSTNode n) {
+        System.out.print(n.data);
+    }
+
+    public void inOrderRecursive(BSTNode n) {
+        if (n == null) {
+            return;
+        }
+        // traverse left
+        inOrderRecursive(n.left);
+        // visit
+        visit(n);
+        // traverse right
+        inOrderRecursive(n.right);
+    }
+
+    public BSTNode search(int x) {
+        // search for the value x in this tree
+        // return the node containing x
+        BSTNode node = root;
+        int comparison = 0;
+        while (node != null) {
+            comparison++;
+            if (node.data == x) {
+                System.out.println("\nNumber of comparisons to find " + x + " is: " + comparison);
+                return node;
+            }
+            if (x < node.data) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        System.out.println("Cannot find " + x + " after " + comparison + " comparison(s)");
+        return null;
     }
 }
--- DSA/src/main.java
+++ DSA/src/main.java
@@ -1,8 +1,5 @@
 import QuickSort.quick_sort;
-import Binary_method.BinaryTree;
-
-import javax.swing.tree.TreeNode;
-
+import QuickSearch.quick_search;
 public class main {
     public static void main(String[] args) {
         // quick_sort
@@ -16,11 +13,8 @@ public class main {
         for (int i = arr.length - 1; i >= 0; i--) {
             System.out.println(arr[i]);
         }
+        // quick_search
         int[] arr2 = { 1, 5, 2, 7, 3, 9, 4, 6, 8 };
-        // setup the binary tree
-        BinaryTree bt = new BinaryTree();
-        TreeNode root = (TreeNode) bt.arrayToBinaryTree(arr2);
-    // binary tree sort
-        pu
+        quick_search.quicksearch(arr2);
     }
 }
